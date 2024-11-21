d81e3433ea1b4751b9649af157eb4036c191ebea
JaroFischer <jaroslawfischer89@gmail.com>
2024-11-21 19:15:21 +0100

    gitignore deleted

---
+++ Source/Lore/Public/Player/LorePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LorePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LORE_API ALorePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
